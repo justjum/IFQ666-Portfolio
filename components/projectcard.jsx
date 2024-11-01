95ce3cfdf79a585654154b395cad7be1d71f51d6
User <jumasaurusrex@gmail.com>
2024-11-02 08:53:50 +1030

    Add links to homepage, image

--- components/projectcard.jsx
+++ components/projectcard.jsx
@@ -4,7 +4,9 @@ import OpenPage from '../images/open-in-new.svg'
 export default function ProjectCard({project}) {
     return  <>
                 <div className={`card project-${project.id}`}>
-                    <img src="project." alt="" />
+                    <a href={project.homepage} target='_blank'>
+                        <img className='card-img-top' src={`https://raw.githubusercontent.com/justjum/${project.name}/master/${project.name}.png`} alt={project.name} />
+                    </a>
                     <div className="card-body">
                         <div className="card-title">
                             <p>{project.name}</p>
@@ -17,7 +19,7 @@ export default function ProjectCard({project}) {
                         <a href={project.html_url} target='_blank'>
                             <img className="icon" src={Github} alt="" />
                         </a>
-                        <a href="">
+                        <a href={project.homepage} target='_blank'>
                             <img className="icon" src={OpenPage} alt="" />
                         </a>
                         
